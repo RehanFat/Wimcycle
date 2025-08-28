266d4bb477892b783fbff0dccb6e5dc86424a3c6
HansFath <akamsisiniea@gmail.com>
2025-08-28 18:14:08 +0700

    Hancycle
    yash

---
+++ Hancycle/Storage/data_sepeda.php
@@ -0,0 +1,60 @@
+<?php 
+
+    namespace Storage;
+
+    $data_sepeda = [
+        [
+            "id" => 1,
+            "merek" => "Polygon",
+            "model" => "Mountain",
+            "harga" => 1700000,
+            "gambar" => "./assets/gunung-grey.jpg",
+            "deskripsi" => "Sepeda Gunung polygon"
+        ],
+
+        [
+            "id" => 2,
+            "merek" => "Polygon",
+            "model" => "Mountain",
+            "harga" => 1700000,
+            "gambar" => "./assets/gunung-grey.jpg",
+            "deskripsi" => "Sepeda Gunung polygon"
+        ],
+
+        [
+            "id" => 3,
+            "merek" => "Polygon",
+            "model" => "Mountain",
+            "harga" => 1800000,
+            "gambar" => "./assets/gunung-grey.jpg",
+            "deskripsi" => "Sepeda Gunung polygon"
+        ],
+
+        [
+            "id" => 4,
+            "merek" => "Polygon",
+            "model" => "BMX",
+            "harga" => 2500000,
+            "gambar" => "./assets/bmx.jpg",
+            "deskripsi" => "Sepeda BMX polygon"
+        ],
+
+        [
+            "id" => 5,
+            "merek" => "Polygon",
+            "model" => "BMX",
+            "harga" => 2700000,
+            "gambar" => "./assets/bmx.jpg",
+            "deskripsi" => "Sepeda BMX polygon"
+        ],
+
+        [
+            "id" => 6,
+            "merek" => "Polygon",
+            "model" => "BMX",
+            "harga" => 2800000,
+            "gambar" => "./assets/bmx.jpg",
+            "deskripsi" => "Sepeda BMX polygon"
+        ],
+    ];
+?>
